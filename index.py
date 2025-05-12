d6cacb7455f5a8ffd5510e2a195cab5ec939400f
Jasdeep Nijjar <jasdeepn4@hotmail.com>
2025-05-12 17:51:54 -0400

    adding readme/license

--- index.py
+++ index.py
@@ -1,1 +1,4 @@
+# Copyright (c) 2025 Jasdeep Nijjar
+# All rights reserved.
+# Commercial use, copying, or redistribution of this software or any substantial portion of it is strictly prohibited without the express written permission of the copyright holder. For commercial licensing, please contact jasdeepn4@gmail.com.
 from app import app
